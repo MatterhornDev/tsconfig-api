81f7a07a787113ea30733368fc31f64de35999c6
Ethan Arrowood <ethan.arrowood@gmail.com>
2019-08-11 16:05:26 -0400

    final commit before merge

--- src/tsconfig-data.ts
+++ src/tsconfig-data.ts
@@ -5,7 +5,24 @@ function isTSConfigKey<T = typeof TSConfigData> (obj: T, key: keyof any): key is
   return key in obj
 }
 
+type TSConfigValue = {
+  cliFlag?: string | string[],
+  type?: string,
+  defaultValue?: string,
+  description?: string,
+  experimental?: boolean,
+  extendedDescription?: string,
+  refLinks?: { title: string, link: string }[]
+}
+type TSConfigKeys = keyof typeof TSConfigData
+type PartialTSConfigData = {
+  [P in TSConfigKeys]?: TSConfigValue
+}
+
 export {
   TSConfigData,
+  TSConfigKeys,
+  TSConfigValue,
+  PartialTSConfigData,
   isTSConfigKey
 }
